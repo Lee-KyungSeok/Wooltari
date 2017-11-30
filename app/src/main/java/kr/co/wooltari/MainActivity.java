ae96591e5fd62b8d53c11f734bf04adb147f50e3
Anya <choi.bosel@gmail.com>
2017-11-30 18:42:28 +0900

    delete MainActivity toolbar function

--- app/src/main/java/kr/co/wooltari/MainActivity.java
+++ app/src/main/java/kr/co/wooltari/MainActivity.java
@@ -17,47 +17,12 @@ import kr.co.wooltari.util.ToolbarUtil;
 
 public class MainActivity extends AppCompatActivity {
 
-//    DrawerLayout drawer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- //       initView();
-    }
 
-//    private void initView(){
-//        // 네비게이션 바 및 툴바 세팅
-//        drawer =findViewById(R.id.drawerMainLayout);
-//        new PetNavigationView(this,drawer, findViewById(R.id.navMainView));
-//        Toolbar toolbar = findViewById(R.id.toolbarMain);
-//        ToolbarUtil.setCommonToolbar(this, toolbar, "");
-//        // 네비게이션 토글 연결
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-//    }
-//
-//    @Override
-//    public void onBackPressed() {
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.common_menu_etc, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        return ToolbarUtil.setMenuItemSelectedAction(this, item) || super.onOptionsItemSelected(item);
-//    }
+    }
 
     //=============================Pet 화면으로 넘기는 임시 버튼===============================
     public void goPet(View v){
