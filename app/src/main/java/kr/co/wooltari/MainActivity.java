6f6947f02475e6484ae74b6d371e989dc0b05216
Anya <choi.bosel@gmail.com>
2017-11-30 18:49:13 +0900

    strange... I did commit mainactivity...

--- app/src/main/java/kr/co/wooltari/MainActivity.java
+++ app/src/main/java/kr/co/wooltari/MainActivity.java
@@ -2,18 +2,11 @@ package kr.co.wooltari;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
-import kr.co.wooltari.custom.PetNavigationView;
 import kr.co.wooltari.pet.PetDetailActivity;
-import kr.co.wooltari.util.ToolbarUtil;
+
 
 public class MainActivity extends AppCompatActivity {
 
