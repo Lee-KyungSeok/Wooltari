e0edb6f6536b8556bdeec8376b8f3135b7542775
Anya <choi.bosel@gmail.com>
2017-12-04 15:54:18 +0900

    confirm activity

--- app/src/main/java/kr/co/wooltari/LoginActivity.java
+++ app/src/main/java/kr/co/wooltari/LoginActivity.java
@@ -1,6 +1,8 @@
 package kr.co.wooltari;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,5 +23,21 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onCLick_Signup(View view){
         startActivity(new Intent(this,SignUpActivity.class));
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Pet")
+                .setMessage("Are you sure you want to close this app?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
