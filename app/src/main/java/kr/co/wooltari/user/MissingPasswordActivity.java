3b4cda99e799cf48460fd183e20bb8d894fa16dc
Anya <choi.bosel@gmail.com>
2017-12-05 15:16:22 +0900

    missing_password Activity완성

--- app/src/main/java/kr/co/wooltari/user/MissingPasswordActivity.java
+++ app/src/main/java/kr/co/wooltari/user/MissingPasswordActivity.java
@@ -2,15 +2,25 @@ package kr.co.wooltari.user;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import kr.co.wooltari.R;
 
 
 public class MissingPasswordActivity extends AppCompatActivity {
-
+    EditText input_email_edittext;
+    Button cancel_button;
+    Button sendemail_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_missing_password);
+        init();
+    }
+    private void init(){
+        input_email_edittext=findViewById(R.id.missingpasswordactivity_input_email_edittext);
+        cancel_button=findViewById(R.id.missingpasswordactivity_cancel_buttoon);
+        sendemail_button=findViewById(R.id.missingpasswordacitivy_sendEmail_button);
     }
 }
