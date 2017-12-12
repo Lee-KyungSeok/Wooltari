f202c12e5add774c2fdbbdc7d1d4525e8cd57205
KyungSeok <leeseok8347@gmail.com>
2017-12-13 03:26:51 +0900

    edit notification

--- app/src/main/java/kr/co/wooltari/alarm/PetAlarmfcm/MyFirebaseMessagingService.java
+++ app/src/main/java/kr/co/wooltari/alarm/PetAlarmfcm/MyFirebaseMessagingService.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -12,6 +14,10 @@ import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import kr.co.wooltari.R;
 import kr.co.wooltari.constant.Const;
@@ -36,12 +42,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         if (remoteMessage.getNotification() != null) {
             sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody()
-                    ,remoteMessage.getNotification().getSound(), remoteMessage.getNotification().getTag());
+                    ,remoteMessage.getNotification().getSound(), remoteMessage.getNotification().getTag(), remoteMessage.getData().get("profile"));
         }
     }
 
     // 노티피케이션 생성
-    private void sendNotification(String title, String content, String sound, String tag) {
+    private void sendNotification(String title, String content, String sound, String tag, String url) {
         Intent intent = new Intent(this, SignInActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, Const.PET_ALARM_NOTIFICATION, intent,
@@ -56,12 +62,24 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 break;
         }
-        int tagId = Integer.parseInt(tag);
+
+        Bitmap largeIcon = null;
+        try {
+            URL imageUrl = new URL(url);
+            largeIcon = BitmapFactory.decodeStream(imageUrl.openConnection().getInputStream());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(largeIcon ==null) largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.pet_detail_default);
 
         String channelId =  "DEFAULT CHANNEL";
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
                         .setColor(ContextCompat.getColor(this,R.color.gold))
+                        .setLargeIcon(largeIcon)
                         .setSmallIcon(R.drawable.pet_profile)
                         .setContentTitle(title)
                         .setContentText(content)
@@ -72,6 +90,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(tagId, notificationBuilder.build());
+        notificationManager.notify(tag, Const.ALARM_NOTI_ID, notificationBuilder.build());
     }
 }
