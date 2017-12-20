8d13ce8d2331e4b78d73fb4ccc6d999195ce959a
KyungSeok <leeseok8347@gmail.com>
2017-12-21 06:00:18 +0900

    edit signin layout

--- app/src/main/java/kr/co/wooltari/user/SignInActivity.java
+++ app/src/main/java/kr/co/wooltari/user/SignInActivity.java
@@ -4,11 +4,14 @@ package kr.co.wooltari.user;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.co.wooltari.R;
+
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
     }
 }
 
